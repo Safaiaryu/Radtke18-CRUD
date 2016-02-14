a0d7dfe533986094e51138e2d89cd7c58bb4300f
Safaiaryu <jacquelineradtke@gmail.com>
2016-02-14 00:50:00 -0500

    Properties

--- SimpleCRUD/Models/Properties.cs
+++ SimpleCRUD/Models/Properties.cs
@@ -5,9 +5,9 @@ using System.Web;
 
 namespace SimpleCRUD.Models
 {
-    public class Properties
-    {
-        public static int MaxPerSeason { get { return 200; } set; }
-        public static string CurrentSeason { get { return "Winter 2016"; } set; }
-    }
+    //public class Properties
+    //{
+    //    public static int MaxPerSeason { get { return 200; } set; }
+    //    public static string CurrentSeason { get { return "Winter 2016"; } set; }
+    //}
 }
